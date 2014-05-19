c1bf469e09fd46f7a46b3b1c7dec7077d2aa5b6e
briandunnington <briandunnington@gmail.com>
2014-05-19 08:46:17 -0700

    initial commit

---
+++ Tests.WindowsPhone/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Tests.WindowsPhone
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
